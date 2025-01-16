3a770c1627f2a7d38baa2f66f3d668bb96cc8234
TheanhPhung <jctaf.smode@gmail.com>
2025-01-16 16:50:42 +0700

    Add and implement login, register views and templates

--- app/views.py
+++ app/views.py
@@ -1,24 +1,65 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth import login, logout
+from django.utils import timezone
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
+from .models import *
 
 def index(request):
-    user = request.user
-    
-    return render(request, "app/index.html", {"user": user})
+    if request.user.is_authenticated:
+        user = request.user
+        return render(request, "app/index.html", {"user": user})
+
+    return redirect("login")
 
 
 def login_view(request):
     logout(request)
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successfull!")
+            return redirect("index")
+
+        else:
+            messages.error(request, "Username or password is not valid!")
+
     return render(request, "app/login.html")
 
 
 def register(request):
     logout(request)
+    if request.method == "POST":
+        username = request.POST["username"]
+        if User.objects.filter(username=username):
+            messsages.error(request, "Username already exists!")
+            return render(request, "app/register.html")
+
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            messages.error(request, "Password does not match!")
+            return render(request, "app/register.html")
+
+        user = User.objects.create_user(username=username, password=password)
+        user.ejaculation_time = timezone.now()
+        user.porn_time = timezone.now()
+        user.masturbation_time = timezone.now()
+        user.save()
+        login(request, user)
+        return redirect("index")
+
     return render(request, "app/register.html")
 
 
+@login_required
 def logout_view(request):
+    username = request.user.username
     logout(request)
-    return HttpResponse("You've logged out!")
+    messages.warning(request, f"You have just logged out of {username}'s account!")
+    return redirect("login") 
