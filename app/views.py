47b45fb6fc3cf026c883bf716d70cc6fb7ca44d8
TheanhPhung <jctaf.smode@gmail.com>
2025-01-16 10:30:30 +0700

    Commit before do the login/register features

--- app/views.py
+++ app/views.py
@@ -1,8 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import login, logout
 
 
 def index(request):
     user = request.user
     
     return render(request, "app/index.html", {"user": user})
+
+
+def login_view(request):
+    logout(request)
+    return render(request, "app/login.html")
+
+
+def register(request):
+    logout(request)
+    return render(request, "app/register.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponse("You've logged out!")
