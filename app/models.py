3d80cbeb8340f829413390d62b74c878fddb553e
TheanhPhung <jctaf.smode@gmail.com>
2025-01-17 10:56:00 +0700

    Optimize some repeated code in 'views' and 'models'

--- app/models.py
+++ app/models.py
@@ -6,38 +6,34 @@ from datetime import date
 
 
 class User(AbstractUser):
+    ACT_LIST = ["ejaculation", "porn", "masturbation"]
+
     birth_date = models.DateField(null=True, blank=True)
     ejaculation_time = models.DateTimeField(null=True, blank=True)
     porn_time = models.DateTimeField(null=True, blank=True)
     masturbation_time = models.DateTimeField(null=True, blank=True)
     level = models.IntegerField(default = 1)
-    ejaculation_target = models.IntegerField(default = 7)
-    porn_target = models.IntegerField(default=7)
-    masturbation_target = models.IntegerField(default=7)
+    ejaculation_target = models.IntegerField(default = 1)
+    porn_target = models.IntegerField(default=1)
+    masturbation_target = models.IntegerField(default=1)
 
     def age(self):
         today = date.today()
         return today.year - self.birth_date.year - ((today.month, today.day) < (self.birth_date.month, self.birth_date.day))
 
-    def non_ejaculation(self):
-        return self.convert_time(timezone.now() - self.ejaculation_time) 
+    def no_action(self, act_code):
+        time_field_name = self.ACT_LIST[act_code] + "_time"
+        time_field_value = getattr(self, time_field_name)
+        return timezone.now() - time_field_value
 
-    def non_porn(self):
-        return self.convert_time(timezone.now() - self.porn_time)
+    def action_progress(self, act_code):
+        time_delta = self.no_action(act_code).days * 86400 + self.no_action(act_code).seconds
+        target_field_name = self.ACT_LIST[act_code] + "_target"
+        target_time = getattr(self, target_field_name) * 86400
+        return time_delta / target_time * 100
 
-    def non_masturbation(self):
-        return self.convert_time(timezone.now() - self.masturbation_time)
-
-    def ejaculation_progress(self):
-        return (self.non_ejaculation()["days"] / self.ejaculation_target) * 100
-
-    def masturbation_progress(self):
-        return (self.non_masturbation()["days"] / self.masturbation_target) * 100
-
-    def porn_progress(self):
-        return (self.non_porn()["days"] / self.porn_target) * 100
-
-    def convert_time(self, time_delta):
+    def convert_time(self, act_code):
+        time_delta = self.no_action(act_code)
         days = time_delta.days
         time_delta = time_delta.seconds + days * 86400
         minutes, seconds = divmod(time_delta, 60)
